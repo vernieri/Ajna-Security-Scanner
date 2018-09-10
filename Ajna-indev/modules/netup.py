c111353da8d4b3addf8cc040b580f60c0df90e17
Vernieri <31966345+vernieri@users.noreply.github.com>
2018-09-10 13:56:22 -0300

    Create netup.py

---
+++ Ajna-indev/modules/netup.py
@@ -0,0 +1,27 @@
+#!/usr/share/python
+import socket, sys
+
+import subprocess
+import os
+
+ip = raw_input("Your IP: ")
+ip.split(".")
+a,b,c,d = ip.split(".")
+
+#nip = a+"."+b+"."+c+"."+"d"
+#print nip
+
+
+
+
+with open(os.devnull, "wb") as limbo:
+        for n in xrange(0, 255):
+
+                #ip="192.168.15.{0}".format(n)
+                nip = a+"."+b+"."+c+"."+"{0}".format(n) 
+                result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2", ip], stdout=limbo, stderr=limbo).wait()
+                if result:
+                        #print "[-] ", nip, "is inactive"
+                        print "\r\n"
+                else:
+                        print "[+] ", nip, "is active"
