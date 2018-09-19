697488c921542bb18ced9bfff856a4b9b7345324
Vernieri <31966345+vernieri@users.noreply.github.com>
2018-09-19 14:18:29 -0300

    Update servicescan.py

--- Ajna-indev/modules/servicescan.py
+++ Ajna-indev/modules/servicescan.py
@@ -3,7 +3,7 @@ import socket, sys
 
 # I AM STILL WORKING ON IT AND IT MAY NOT WORK FOR ALL SERVICES
 ip = raw_input("Target IP: ")
-port = input("Target Port: \r\n")
+port = input("Target Port: ")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
