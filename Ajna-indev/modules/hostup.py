852ac94ff37e46e9b69a548034049e4df8f2ce68
Vernieri <jvmendes.ti@gmail.com>
2018-10-16 14:39:42 -0300

    Update hostup.py

--- Ajna-indev/modules/hostup.py
+++ Ajna-indev/modules/hostup.py
@@ -3,8 +3,8 @@ import sys
 import os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Create a TCP/IP socket
-server_ip = raw_input("Enter server IP : ")
-rep = os.system("ping " + server_ip)
+server_ip = raw_input("Target IP : ")
+rep = os.system("ping -c 1 " + server_ip)
 
 if rep == 0:
     print "server is up"
