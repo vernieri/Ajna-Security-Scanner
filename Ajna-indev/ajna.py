2e20ad3ca1492dcf40c409dbded071450941446e
Vernieri <jvmendes.ti@gmail.com>
2018-10-18 14:01:21 -0300

    Update ajna.py

--- Ajna-indev/ajna.py
+++ Ajna-indev/ajna.py
@@ -12,7 +12,7 @@ from pyfiglet import figlet_format
 def main():
 	screen()
 	print "AJNA is a Security Scanner!"
-	print "       Version: a0.64"
+	print "       Version: a0.75"
 	print "           Made By: Vernieri\r\n"
 
 	print "--------------{0}--------------"
