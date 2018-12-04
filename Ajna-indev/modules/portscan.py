801270fea1375e64404b944de547e65871f17e22
Vernieri <jvmendes.ti@gmail.com>
2018-12-04 17:02:04 -0200

    Update portscan.py

--- Ajna-indev/modules/portscan.py
+++ Ajna-indev/modules/portscan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import socket
 # Working in a Multi-Thread Mode to optimize this...
+#Let's get started
 ip = raw_input("Target IP: ")
 for port in range(1,65535):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
