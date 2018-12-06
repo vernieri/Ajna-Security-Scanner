75d7f4ce7a107b935c85f2ed15f6949ee2812f1c
Vernieri <jvmendes.ti@gmail.com>
2018-12-06 16:06:04 -0200

    Update portscan.py

--- Ajna-indev/modules/portscan.py
+++ Ajna-indev/modules/portscan.py
@@ -16,3 +16,12 @@ def portscan1():
 			time.sleep(0.1)
 			print "[+]",port," is Open!"
 			s.close
+
+def portscan2():
+	#ip = raw_input("Target IP: ")
+	for port in range(32768,65535):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		if s.connect_ex((ip, port))==0:
+			time.sleep(0.1)
+			print "[+]",port," is Open!"
+			s.close					
