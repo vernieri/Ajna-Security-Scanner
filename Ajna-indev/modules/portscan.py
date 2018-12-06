85769dd6af751c699bcda6101b4f77031c0c8058
Vernieri <jvmendes.ti@gmail.com>
2018-12-06 16:05:51 -0200

    Update portscan.py

--- Ajna-indev/modules/portscan.py
+++ Ajna-indev/modules/portscan.py
@@ -7,3 +7,12 @@ import time
 # Maybe doing all this C++ will be better...i'm studying about it.
 
 ip = raw_input("Target IP: ")
+
+def portscan1():
+	#ip = raw_input("Target IP: ")
+	for port in range(1,32767):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		if s.connect_ex((ip, port))==0:
+			time.sleep(0.1)
+			print "[+]",port," is Open!"
+			s.close
