dd1fb16aa3df64ffc1029f3d2e33dc0e4e5f7bbe
Vernieri <jvmendes.ti@gmail.com>
2019-01-23 15:49:37 -0200

    Update servicescan.py

--- Ajna-indev/modules/servicescan.py
+++ Ajna-indev/modules/servicescan.py
@@ -1,12 +1,10 @@
-#!/usr/share/python
-import socket, sys
+#!/usr/bin/env python
+import nmap # import nmap.py module
+nm = nmap.PortScanner() # instantiate nmap.PortScanner object
 
-# I AM STILL WORKING ON IT AND IT MAY NOT WORK FOR ALL SERVICES
 ip = raw_input("Target IP: ")
-port = input("Target Port: ")
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ip, port))
-s.send("What's up\r\n")
-resp = s.recv(1024)
-print resp
+port = raw_input("Port or range of ports: ")
+nm.scan(ip, port) # scan host 127.0.0.1, ports from 22 to 443
+print('----------------------------------------------------')
+# print result as CSV
+print(nm.csv())
