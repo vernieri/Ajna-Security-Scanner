078313cb2c946f2d0ff2a0a35c4ce93ec9e726eb
Vernieri <jvmendes.ti@gmail.com>
2019-06-05 09:24:03 -0300

    Update main.go

--- Ajna-GoLang/Ajna/main.go
+++ Ajna-GoLang/Ajna/main.go
@@ -19,3 +19,22 @@ func main() {
 	parserArgs(args)
 
 }
+
+func parserArgs(args []string) string{
+	var d string
+	p(args)
+	pArgs := strings.Join(args, "\n")
+	//p(pArgs)
+	s := strings.Split(pArgs, "\n")
+	p(s[1])
+	
+	//s := strings.Split(pArgs, "./Ajna")
+	//p(s)
+	//x := strings.Join(s, " ")
+	//p(x)
+	//first := strings.Split(x, "-")
+	//p(first)
+	
+
+	return d
+}
